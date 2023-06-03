18f9ebf713e402a721a4fc60a11c6c846a962a68
Eric <erho20@gmail.com>
2023-06-03 12:54:55 -0700

    Adds retrofit dependencies and sets up retrofit object (hardcoding url for now)

---
+++ app/src/main/java/com/example/nasasearch/network/NASASearchApiService.kt
@@ -0,0 +1,23 @@
+package com.example.nasasearch.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://images-api.nasa.gov/"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NASASearchApiService {
+    @GET("search?q=mars&media_type=image")
+    fun getData(): String
+}
+
+object NASAApi {
+    val retrofitService : NASASearchApiService by lazy {
+        retrofit.create(NASASearchApiService::class.java)
+    }
+}
