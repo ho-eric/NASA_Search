7e063e4584ed3a00c8c71217c69b335f40e96fe8
Eric <erho20@gmail.com>
2023-06-04 16:58:59 -0700

    Adds title on top of images

--- app/src/main/java/com/example/nasasearch/ui/theme/theme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/nasasearch/ui/theme/theme/screens/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.example.nasasearch.ui.theme.theme.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,19 +15,32 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.nasasearch.R
@@ -39,11 +54,43 @@ fun HomeScreen(
     retryAction: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var text by remember { mutableStateOf("") }
+
     when (nasaUiState) {
+//        is NASAUiState.HomeSearch -> SearchScreen(modifier = modifier)
         is NASAUiState.Loading -> LoadingScreen(modifier = modifier)
         is NASAUiState.Success -> PhotosGridScreen(nasaUiState.photos, modifier = modifier)
         is NASAUiState.Error -> ErrorScreen(retryAction, modifier)
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    Column {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = text,
+            onQueryChange = {
+                text = it
+            },
+            onSearch = {
+                active = false
+            },
+            active = active,
+            onActiveChange = {
+                active = it
+            },
+            placeholder = {
+                Text(text = "Search...")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+            }
+        ) {
+        }
     }
 }
 
@@ -75,19 +122,10 @@ fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
     }
 
 }
-@Composable
-fun ResultScreen(nasaUiState: String, modifier: Modifier = Modifier) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Text(nasaUiState)
-    }
-}
 
 @Composable
 fun NASAPhotoCard(photo: Item, modifier: Modifier = Modifier) {
-    Card (
+    Card(
         modifier = modifier
             .padding(4.dp)
             .fillMaxWidth()
@@ -96,37 +134,94 @@ fun NASAPhotoCard(photo: Item, modifier: Modifier = Modifier) {
             defaultElevation = 8.dp
         ),
 
-        ){
-        AsyncImage(
-            model = ImageRequest.Builder(context = LocalContext.current)
-                .data(photo.links[0].href)
-                .crossfade(true)
-                .build(),
-            error = painterResource(R.drawable.ic_broken_image),
-            placeholder = painterResource(R.drawable.loading_img),
-            contentDescription = "Test",
-            contentScale = ContentScale.FillBounds
-        )
+        ) {
+        Box {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data(photo.links[0].href)
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img),
+                contentDescription = "Test",
+                contentScale = ContentScale.FillBounds
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 100f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = photo.data[0].title,
+                    style = TextStyle(color = Color.White, fontSize = 15.sp)
+                )
+            }
+        }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhotosGridScreen(photos: Collection, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
-        modifier = modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(4.dp)
-    ) {
-        items(items = photos.collection.items, key = {photos -> photos.data[0].nasaId}) {
-            photo -> NASAPhotoCard(photo)
+    var searchTerm by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    Column {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth(),
+            query = searchTerm,
+            onQueryChange = {
+                searchTerm = it
+            },
+            onSearch = {
+                active = false
+            },
+            active = active,
+            onActiveChange = {
+                active = it
+            },
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search icon")
+            }
+        ) {
+        }
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(150.dp),
+            modifier = modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(4.dp)
+        ) {
+            items(
+                items = photos.collection.items,
+                key = { photos -> photos.data[0].nasaId }) { photo ->
+                NASAPhotoCard(photo)
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ResultScreenPreview() {
     NASASearchTheme {
-
     }
 }
